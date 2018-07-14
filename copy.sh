f187d7ab9f95f8d04cd0ba60bfa4a46232c95470
Tyler Hatzenbuhler <t.hatzenbuhler@gmail.com>
2018-07-14 13:06:41 -0700

    changed dir

--- copy.sh
+++ copy.sh
@@ -2,12 +2,12 @@
 #Comment test
 echo "Copying dotfiles..."
 
-cp ~/.i3blocks.conf ~/Documents/OldDotfiles/LinuxDotfiles/
-cp ~/.vimrc ~/Documents/OldDotfiles/LinuxDotfiles/
-cp ~/.Xdefaults ~/Documents/OldDotfiles/LinuxDotfiles/
-cp ~/.zshrc ~/Documents/OldDotfiles/LinuxDotfiles/
-cp -R ~/.config/i3 ~/Documents/OldDotfiles/LinuxDotfiles/.config
-cp -R ~/.config/ranger ~/Documents/OldDotfiles/LinuxDotfiles/.config
-cp -R ~/.config/polybar ~/Documents/OldDotfiles/LinuxDotfiles/.config
+cp ~/.i3blocks.conf ~/Documents/Dotfiles/LinuxDotfiles/
+cp ~/.vimrc ~/Documents/Dotfiles/LinuxDotfiles/
+cp ~/.Xdefaults ~/Documents/Dotfiles/LinuxDotfiles/
+cp ~/.zshrc ~/Documents/Dotfiles/LinuxDotfiles/
+cp -R ~/.config/i3 ~/Documents/Dotfiles/LinuxDotfiles/.config
+cp -R ~/.config/ranger ~/Documents/Dotfiles/LinuxDotfiles/.config
+cp -R ~/.config/polybar ~/Documents/Dotfiles/LinuxDotfiles/.config
 
 echo "Done."
