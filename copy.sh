c5323c51149b1eb11548b27955c380a08315c72f
Tyler Hatzenbuhler <t.hatzenbuhler@gmail.com>
2018-07-14 13:02:03 -0700

    sh

--- copy.sh
+++ copy.sh
@@ -5,7 +5,7 @@ echo "Copying dotfiles..."
 cp ~/.i3blocks.conf ~/Documents/OldDotfiles/LinuxDotfiles/
 cp ~/.vimrc ~/Documents/OldDotfiles/LinuxDotfiles/
 cp ~/.Xdefaults ~/Documents/OldDotfiles/LinuxDotfiles/
-cp ~/.zshrc ~/Documents/Dotfiles/LinuxDotfiles/
+cp ~/.zshrc ~/Documents/OldDotfiles/LinuxDotfiles/
 cp -R ~/.config/i3 ~/Documents/OldDotfiles/LinuxDotfiles/.config
 cp -R ~/.config/ranger ~/Documents/OldDotfiles/LinuxDotfiles/.config
 cp -R ~/.config/polybar ~/Documents/OldDotfiles/LinuxDotfiles/.config
