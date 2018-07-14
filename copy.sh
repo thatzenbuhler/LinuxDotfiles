fbda9664b7746652192385a1a3c56b86bc2f25c4
Tyler Hatzenbuhler <t.hatzenbuhler@gmail.com>
2018-07-14 12:51:31 -0700

    Changed for new Arch install

---
+++ copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Comment test
+echo "Copying dotfiles..."
+
+cp ~/.i3blocks.conf ~/Documents/OldDotfiles/LinuxDotfiles/
+cp ~/.vimrc ~/Documents/OldDotfiles/LinuxDotfiles/
+cp ~/.Xdefaults ~/Documents/OldDotfiles/LinuxDotfiles/
+cp ~/.zshrc ~/Documents/Dotfiles/LinuxDotfiles/
+cp -R ~/.config/i3 ~/Documents/OldDotfiles/LinuxDotfiles/.config
+cp -R ~/.config/ranger ~/Documents/OldDotfiles/LinuxDotfiles/.config
+cp -R ~/.config/polybar ~/Documents/OldDotfiles/LinuxDotfiles/.config
+
+echo "Done."
